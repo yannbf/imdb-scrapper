5c1f0e2cee2c35f20690ed12a8829074e5943562
Yann Braga <yannbf@gmail.com>
2017-10-02 17:32:55 -0300

    add netflix part

--- index.js
+++ index.js
@@ -4,86 +4,70 @@ var request      = require('request');
 var cheerio      = require('cheerio');
 var app          = express();
 
-app.get('/trivia', function(req, res){
-    var id = req.query.id;
-    url    = 'http://www.imdb.com/title/' + id + '/trivia';
-
-    console.log('Starting request..');
-    request(url, function(error, response, html){
-        if(!error){
-            console.log('Request done with no errors!');
-            var $ = cheerio.load(html), json = {};
-
-            var commonList  = $("#trivia_content > .list").parent().find("div.sodatext");
-            var cameoList   = $("#trivia_content .list > a#cameo").parent().find("div.sodatext");
-            var spoilerList = $("#trivia_content .list > a#spoilers").parent().find("div.sodatext");
-
-            json.common   = extractData(commonList);
-            json.cameo    = extractData(cameoList);
-            json.spoiler  = extractData(spoilerList);
-        }
-
-        function extractData(list){
-            console.log(list);
-            var data = [];
-            list.each(function(i,e){
-                data.push($(this).text().trim());
-            });
-
-            return data;
-        }
-
-        writeFile('trivia_' + id, json);
-
-        res.send(json);
-    });
-});
 
 app.get('/scrape', function(req, res){
-    var id = req.query.id;
-    url    = 'http://www.imdb.com/title/' + id;
-    request(url, function(error, response, html){
-        if(!error){
-            console.log('Request done with no errors!');
-            var $ = cheerio.load(html);
-
-            var title, release, rating, trivia;
-            var json = { title : "", storyLine : "", imdbRating : "", releaseDate: "", duration: "", awards: "" };
-
-            title       = $('.originalTitle').text().replace('(original title)','');
-            storyLine   = $("#titleStoryLine").find("div.inline.canwrap p").text();
-            imdbRating  = $("div.ratingValue > strong > span").text();
-            releaseDate = $('meta[itemprop=datePublished]').attr("content");
-            duration    = $('time[itemprop=duration]').first().text();
-            awards      = $('span[itemprop=awards]').last().text()
-                          + $('span[itemprop=awards]').last().text();
-
-            json.title       = title.trim();
-            json.storyLine   = storyLine.trim();
-            json.imdbRating  = imdbRating.trim();
-            json.releaseDate = releaseDate.trim();
-            json.duration    = duration.trim();
-            json.awards      = awards.trim();
-        }
-
-        writeFile(id, json);
-
-        res.send(json);
-    });
+    const ids = req.query.id.split(',');
+    makeRequest(ids[0], req, res);
+    // ids.forEach(id => makeRequest(id, req, res));
 });
 
+function makeRequest(id, req, res) {
+    const url = 'http://instantwatcher.com/title/' + id;
+    request(url, (error, response, html) => {
+        if(!error){
+            console.log('Request done with no errors!');
+            const $ = cheerio.load(html);
+
+            const titles = $('.netflix-title');
+
+            const mainInfo = titles[1];
+            const actors = $('.actors a');
+
+            var serie = {
+                id       : id,
+                poster   : {
+                    portrait: $('.iw-boxart').attr('src'),
+                    landscape: ''
+                },
+                title    : $(mainInfo).find('.title-link').text(),
+                year     : $(mainInfo).find('.year a').text(),
+                synopsis : $(mainInfo).find('.synopsis').text(),
+                cast     : $(actors[0]).text() + ', ' + $(actors[1]).text() + ', ' + $(actors[2]).text(),
+                episodes : []
+            }
+
+            titles.splice(0,2);
+
+            titles.each((index, episode) => {
+                var episodeData = {
+                    number   : (index + 1),
+                    title    : $(episode).find('.episode-title').text(),
+                    synopsis : $(episode).find('.synopsis').text(),
+                    runtime  : $(episode).find('.runtime').text(),
+                    link     : $(episode).find('.action-play').attr('href')
+                }
+                serie.episodes.push(episodeData);
+            });
+        }
+
+        // writeFile(id, serie);
+
+        res.send(serie);
+    });
+}
+
 function writeFile(name, file){
-    var dir = './data/';
+    const dir = './data/';
 
     if (!fs.existsSync(dir)){
         fs.mkdirSync(dir);
     }
 
-    var filename = dir + name + '.json';
+    const filename = `${dir}${name}.json`;
 
-    fs.writeFile(filename, JSON.stringify(file, null, 4), function(err){
-        console.log('File successfully written: ' + filename);
-    });
+    fs.writeFile(filename, JSON.stringify(file, null, 4), err =>
+        console.log('File successfully written: ' + filename)
+    );
 }
 
 app.listen('8081')
